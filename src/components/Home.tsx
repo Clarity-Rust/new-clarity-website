2060bca85943dc1f197f8c174e41676dbc337931
anuraag <anuraag.warudkar@gmail.com>
2024-03-28 12:39:49 -0400

    attempt to fix home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,19 @@
 import "./Home.css";
-// import { Button } from "./ui/button";
+import { Button } from "./ui/button";
 // import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <div className="background">
-      <div className="flex h-screen items-center justify-center">
-        <div className="grid grid-cols-3 gap-4"></div>
+      <div className="flex h-screen items-center justify-center flex-col my-5 home-main">
+        <div>
+          <img src="/home-logo.png" width={600} />
+        </div>
+        <div className="flex gap-4">
+          <Button size="lg">Store</Button>
+          <Button size="lg">Discord</Button>
+          <Button size="lg">Steam</Button>
+        </div>
       </div>
     </div>
   );
